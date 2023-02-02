af4215afbee0854c1bcbb99e9de2746c56bba78e
Elena Blagodir <elenalx28@gmail.com>
2023-02-02 15:36:00 +0200

    10

--- src/js/fetchCountries.js
+++ src/js/fetchCountries.js
@@ -8,12 +8,3 @@ export function fetchCountries(name) {
   });
 }
 
-// const BASE_URL = 'https://restcountries.com/v3.1/name/';
-// // іменований експорт функції
-// export function fetchCountries(name) {
-//   return fetch(
-//     `${BASE_URL}${name}?fields=name,capital,population,flags,languages`
-//   ).then(response => {
-//     console.log(response);
-//   });
-// }
