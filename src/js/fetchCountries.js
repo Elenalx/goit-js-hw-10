090aeb56c85003eb016748d028e3ad7941ec2412
Elena Blagodir <elenalx28@gmail.com>
2023-02-02 17:16:23 +0200

    Number 10

--- src/js/fetchCountries.js
+++ src/js/fetchCountries.js
@@ -1,10 +1,12 @@
 const API_URL = 'https://restcountries.com/v3.1/name/';
-
 export function fetchCountries(name) {
-  return fetch(
-      `${API_URL}${name}?fields=name,capital,population,flags,languages`
-).then(response => {
-    console.log(response);
-  });
-}
 
+  return fetch(
+    `${API_URL}${name}?fields=name,capital,population,flags,languages`)
+  .then(response => {
+    if (!response.ok) {
+      throw new Error(response.status);
+    }
+    return response.json();
+  })
+}
